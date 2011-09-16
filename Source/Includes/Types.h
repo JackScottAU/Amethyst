110a7872fc887e8ccf051badd680d08307a2e419
Jack Scott <jack@jackscott.id.au>
2011-09-16 21:42:12 +1000

    Found an empty types file on my hard drive. Adding it.

---
+++ Source/Includes/Types.h
@@ -0,0 +1,16 @@
+/**
+	New Standard Library - Standard Types
+*/
+
+#ifndef __STDTYPES_H
+#define __STDTYPES_H
+
+//Basic Type Definitions.
+typedef	signed		char		sint8;
+typedef	unsigned	char		uint8;
+typedef signed		short		sint16;
+typedef unsigned	short		uint16;
+typedef signed		int			sint32;
+typedef unsigned	int			uint32;
+
+#endif
