63cd818ba1f62ebc5d76dca4e042a6648780c26b
Jack Scott <jack@jackscott.id.au>
2011-10-20 20:56:39 +1100

    Removed print messages from the kernel that won't work, and never did.

--- Source/kernel.c
+++ Source/kernel.c
@@ -24,30 +24,20 @@ void kernel_initialise(uint32 magicNumber, void* multibootData)
 	vgaConsole_printf("Hex: %h\n",0xCAFEBABE);
 	vgaConsole_printf("Status 1: %s\n",1);
 	vgaConsole_printf("Status 2: %s\n",0);
-	/*	clear_screen(WHITE_TXT);
-	k_printf("Welcome to YakOS v0.01", 0, WHITE_TXT);	//Welcome Screen
-	k_printf("----------------------", 1, WHITE_TXT);
-	update_cursor(1,21);
+
+	/*INTS(0);
 	
-	k_printf("Disabling Interrupts...",2,WHITE_TXT);
-	INTS(0);
-	
-	k_printf("Now Remapping PICs starting at 0x20...",3,WHITE_TXT);
 	remap_pics(0x20,0x28);
 	
-	k_printf("Masking all IRQs",4,WHITE_TXT);
 	maskIRQ(0xFF);
 	
-	k_printf("Filling IDT with Interrupts",5,WHITE_TXT);
 	CreateInterrupts();
 	
-	k_printf("Loading IDT",6,WHITE_TXT);
 	loadIDTR();
 	
-	k_printf("Masking all IRQs",7,WHITE_TXT);
 	unmaskIRQ(0xFD);
- * 
- * 	//Init kernel core.
+
+	//Init kernel core.
 	gdtInstall();
 	IDT_Init();
 	ISRs_Install();		//} These two could be done in any order.
@@ -59,11 +49,7 @@ void kernel_initialise(uint32 magicNumber, void* multibootData)
 	//The moment of truth.
 	IDT_EnableInts();
 	
-	k_printf("Enabling Interrupts...",8,WHITE_TXT);
-	if(INTS(1))
-		k_printf("INTS enabled",9,WHITE_TXT);
-	
-	k_printf("Now try pressing something on the keyboard...",10,WHITE_TXT); */
+	INTS(1);*/
 	
 	while(1)
 	{
