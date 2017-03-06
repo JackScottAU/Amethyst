0af2f9e452cf9c63a65275570f9a8502bffe02a8
Jack Scott <jack@jackscott.id.au>
2017-03-06 21:46:14 +1100

    Fixed PCI bus search not being able to read class register.

--- Source/pciBus.c
+++ Source/pciBus.c
@@ -54,23 +54,26 @@ void pci_checkSlot(uint8 bus, uint8 slot)
 	
 	for(function = 0; function < 32; function++)
 	{
-		uint32 manuslot = pci_readConfigurationRegister(bus, slot, function, 0);
+		uint32 deviceAndVendor = pci_readConfigurationRegister(bus, slot, function, 0x00);
 		
-		if((manuslot & 0xFFFF) == 0xFFFF)
+		if((deviceAndVendor & 0xFFFF) == 0xFFFF)
 			break;
+
+        uint32 classRegister = pci_readConfigurationRegister(bus, slot, function, 0x08);
+		uint8 class = (uint8) (classRegister >> 24);
 		
-		vgaConsole_printf("Found PCI device at %d:%d:%d with vendor %h and device %h\n",bus,slot,function,manuslot&0xFFFF,manuslot>>16);
-		//uint32 classRegister = pci_readConfigurationRegister(bus, slot, function, 0x08);
-		//uint8 class = (uint8) classRegister >> 24;
-		//vgaConsole_printf("it has class %h\n",classRegister);
-		//vgaConsole_printf(classNames[class]);
+		vgaConsole_printf("PCI device at %d:%d:%d has vendor %h and device %h, of class ",bus,slot,function,deviceAndVendor&0xFFFF,deviceAndVendor>>16);
+        vgaConsole_printf(classNames[class]);
+		
+		vgaConsole_printf(".\n");
+		
 	}
 }
 
 uint32 pci_readConfigurationRegister(uint8 bus, uint8 slot, uint8 function, uint8 registerNo)
 {
 	//Create the address of the register we want to read from. This is an address in "PCI configuration space", *not* I/O space or general memory space.
-	uint32 address = 0x80000000 | (uint32)((bus & 0xFF)<<16) | (uint32)((slot & 0x1F)<<11) | (uint32)((function & 0x5)<<8) | (uint32)((registerNo & 0x2F)<<2);
+	uint32 address = 0x80000000 | (uint32)((bus & 0xFF)<<16) | (uint32)((slot & 0x1F)<<11) | (uint32)((function & 0x5)<<8) | (uint32)((registerNo & 0xFC));
 	
 	//Write the address of the register we want to the access request I/O port.
 	writeLong(0xCF8, address);
